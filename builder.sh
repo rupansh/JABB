c37623e41289f548182aa156dd59f85915dfa6cd
Rupansh Kek <rupanshsekar@hotmail.com>
2018-11-06 00:50:54 +0530

    builder.sh: Add custom command support

--- builder.sh
+++ builder.sh
@@ -2,13 +2,26 @@
 
 . bu*/e*
 
-if [ $2 != 'clean' ]
+if [ -z $2 ]
 then
-brunch $1
+brunch $1 > log.txt
 
-else
+elif [ $2 == 'clean' ]
+then
 make clean
 make clobber
-brunch $1
+brunch $1 > log.txt
+
+elif [[ $2 == 'custom' ]] && [[ $3 != 'clean' ]]
+then
+lunch $1
+eval '$jabbcmd' > log.txt
+
+elif [[ $2 == 'custom' ]] && [[ $3 == 'clean' ]]
+then
+make clean
+make clobber
+lunch $1
+eval '$jabbcmd' > log.txt
 
 fi
